595a7c463c26a93090240e0a47e837afc26990ca
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-16 10:43:29 -0300

    Curso de Python 3: Avançando na orientação a objetos Aula 03
    Polimorfismo

--- modelo.py
+++ modelo.py
@@ -19,17 +19,29 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'Nome: {self.nome} - Likes: {self.likes}'
+
+    def __repr__(self):
+        return f'{{{self.__class__.__name__}}} {self}'
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -41,5 +53,9 @@ atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+listinha = [atlanta, vingadores]
+
+for programa in listinha:
+    print(programa)
+    print(programa.__repr__())
+
