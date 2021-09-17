b18f7d6cc1552091ea1e9c6adb00daaa68b9d0da
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-17 09:59:42 -0300

    Curso de Python 3: Avançando na orientação a objetos Aula 05

--- modelo.py
+++ modelo.py
@@ -44,18 +44,45 @@ class Serie(Programa):
         return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-listinha = [atlanta, vingadores]
+listinha = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', listinha)
 
-for programa in listinha:
+for programa in minha_playlist.listagem:
     print(programa)
-    print(programa.__repr__())
 
+print(f'Tamanho: {len(minha_playlist.listagem)}')COP
